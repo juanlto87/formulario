58da4455e36e4aaf621491b9bd03bd3b6efc5324
= <juanito.campoverde@gmail.com>
2025-01-10 17:33:26 -0500

    20250110

--- pages/Ventas.jsx
+++ pages/Ventas.jsx
@@ -1,5 +1,5 @@
 import classes from "./Ventas.module.css";
-import {useState, useEffect} from "react";
+import {useState, useEffect, useRef} from "react";
 
 export default function Ventas() {
   //const servicio = "https://blackbox--blackbox-abeb3.us-central1.hosted.app/";
@@ -7,7 +7,12 @@ export default function Ventas() {
   function guardarVenta() {}
   const [productos, setProductos] = useState([]);
   const [inputValue, setInputValue] = useState("");
-  const [suggestions, setSuggestions] = useState([]);
+  const [sugerencias, setSugerencias] = useState([]);
+  const [pedidos, setPedidos] = useState([]);
+  const cantidad = useRef();
+  const precio = useRef();
+  const subtotal = useRef();
+  const total = useRef();
 
   useEffect(() => {
     fetch(`${servicio}productos/`)
@@ -15,7 +20,6 @@ export default function Ventas() {
       .then((data) => setProductos(data))
       .catch((error) => setError("Error al cargar los productos"));
   }, []);
-  const options = ["Active", "Agua", "Gatorade", "Monster"];
 
   // Manejar cambios en el input
   const handleInputChange = (e) => {
@@ -24,77 +28,151 @@ export default function Ventas() {
 
     // Filtrar opciones basadas en el input
     if (value) {
-      const filteredSuggestions = options.filter((option) =>
-        option.toLowerCase().startsWith(value.toLowerCase())
+      const filteredSuggestions = productos.filter((option) =>
+        option.nombre.toLowerCase().startsWith(value.toLowerCase())
       );
-      setSuggestions(filteredSuggestions);
+      setSugerencias(filteredSuggestions);
     } else {
-      setSuggestions([]);
+      setSugerencias([]);
     }
   };
 
   // Manejar selección de una sugerencia
   const handleSuggestionClick = (suggestion) => {
-    setInputValue(suggestion);
-    setSuggestions([]);
+    setInputValue("");
+    setSugerencias([]);
+    suggestion.cantidad = 1;
+    suggestion.subtotal = suggestion.precio * suggestion.cantidad;
+    setPedidos((prev) => {
+      const productoExistente = prev.some(
+        (item) => item.nombre === suggestion.nombre
+      );
+      if (productoExistente) return prev;
+      return [suggestion, ...prev];
+    });
   };
+
+  function handleAgregar() {
+    console.log("Agregar");
+  }
+  function handleSubtotal(pedid, campo, valor) {
+    pedid[campo] = valor;
+    setPedidos((prev) => {
+      const nuevosPedidos = prev.map((pedido) =>
+        pedido.id === pedid.id
+          ? {
+              ...pedido,
+              [campo]: +valor,
+              subtotal: pedid.cantidad * pedid.precio,
+            }
+          : pedido
+      );
+
+      total.current.value = nuevosPedidos.reduce(
+        (acumulador, producto) => acumulador + producto.subtotal,
+        0
+      );
+      return nuevosPedidos;
+    });
+  }
+
+  useEffect(() => {
+    total.current.value = pedidos
+      .reduce((acumulador, producto) => acumulador + producto.subtotal, 0)
+      .toFixed(2);
+  }, [pedidos]);
 
   return (
     <>
-      <div style={{position: "relative", width: "300px"}}>
+      <div className={classes.ventas}>
         <h2>Lista de Ventas</h2>
-        <form onSubmit={guardarVenta}>
-          <p>
-            <label>Nombre: </label>
-            <input placeholder="Nombre " type="text"></input>
-          </p>
-          <p>
-            <label>Proucto:</label>
-            <input
-              type="text"
-              value={inputValue}
-              onChange={handleInputChange}
-              placeholder="Buscar artículo..."
-              style={{width: "80%", padding: "8px"}}
-            />
-            {suggestions.length > 0 && (
-              <ul
-                style={{
-                  position: "absolute",
-                  top: "36px",
-                  left: "36px",
-                  width: "80%",
-                  background: "#fff",
-                  border: "1px solid #ccc",
-                  listStyle: "none",
-                  padding: "0",
-                  margin: "0",
-                  maxHeight: "150px",
-                  overflowY: "auto",
-                  zIndex: 1000,
-                  color: "#000",
-                }}
+
+        <p>
+          <label>Nombre: </label>
+          <input placeholder="Nombre " type="text" />
+        </p>
+        <p>
+          <label>Producto:</label>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="Buscar artículo..."
+          />
+          <label onClick={handleAgregar}>Agregar</label>
+        </p>
+        {sugerencias.length > 0 && (
+          <ul className={classes.ul}>
+            {sugerencias.map((suggestion, index) => (
+              <li
+                key={index}
+                className={classes.li}
+                onClick={() => handleSuggestionClick(suggestion)}
               >
-                {suggestions.map((suggestion, index) => (
-                  <li
-                    key={index}
-                    onClick={() => handleSuggestionClick(suggestion)}
-                    style={{
-                      padding: "8px",
-                      cursor: "pointer",
-                      borderBottom: "1px solid #f0f0f0",
-                    }}
-                  >
-                    {suggestion}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </p>
-          <p>
-            <button>Guardar</button>
-          </p>
-        </form>
+                {suggestion.nombre}: ${suggestion.precio}
+              </li>
+            ))}
+          </ul>
+        )}
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Producto</th>
+                <th>Cantidad</th>
+                <th>$Precio</th>
+                <th>$Subtotal</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pedidos.map((pedido) => (
+                <tr key={pedido.id}>
+                  <td>{pedido.nombre}</td>
+                  <td>
+                    <input
+                      type="number"
+                      value={pedido.cantidad}
+                      onChange={(e) =>
+                        handleSubtotal(pedido, "cantidad", e.target.value)
+                      }
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="number"
+                      onChange={(e) =>
+                        handleSubtotal(pedido, "precio", e.target.value)
+                      }
+                      min="1"
+                      step="0.01"
+                      value={pedido.precio.toFixed(2)}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="number"
+                      value={pedido.subtotal.toFixed(2)}
+                      disabled
+                    />
+                  </td>
+                </tr>
+              ))}
+              {total && (
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td>Total:</td>
+                  <td>
+                    <input type="number" ref={total} disabled />
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+        <p>
+          <button>Guardar</button>
+        </p>
       </div>
     </>
   );
